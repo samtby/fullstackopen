14745310a83e1976b8b12b013b31dea47a5ada73
NoRoc <samtby@hydrox.fr>
2021-06-23 08:17:30 +0200

    Debugging React applications

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -170,6 +170,32 @@ const App = (props) => {
 //
 // d A more complex state, debugging React apps
 // ----------------------------------------------------------------------
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+    return (
+      <di>
+          button press history: {props.allClicks.join(' ')}
+      </di>
+    )
+}
+
+const Button = (props) => { 
+  console.log('props value is', props)
+  const { onClick, text } = props
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -185,18 +211,14 @@ const App = () => {
     setRight(right + 1)
 
   }
-
+  
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text='left' />      
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
