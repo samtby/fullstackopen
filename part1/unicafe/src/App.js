d97658bba2c0d09b5a600c0515984e271bbd9b91
NoRoc <samtby@hydrox.fr>
2021-06-24 19:23:08 +0200

    1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react'
 //The application must display the total number of collected feedback for each category. 
 const Display = props => <h1>{props.value}</h1>
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-     {props.text}
-  </button>
+const Button = ({handleClick,text}) => (
+  <button onClick={handleClick}>{text}</button>
 )
 
-const Feedback =  (props) => (
-  <p>{props.text} {props.numfeedback}</p>
+const Feedback =  ({text,numFeedBack}) => (
+  <p>{text} {numFeedBack}</p>
 )
 
 const App = () => {
@@ -18,20 +16,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const SetIncreaseByOneGood = () => setGood(good + 1)
-  const SetIncreaseByOneNeutral = () => setGood(neutral + 1)
-  const SetIncreaseByOneBad = () => setGood(bad + 1)
+  const setIncreaseByOneGood = () => setGood(good + 1)
+  const setIncreaseByOneNeutral = () => setNeutral(neutral + 1)
+  const setIncreaseByOneBad = () => setBad(bad + 1)
   
   return (
     <div>
       <Display value="give feedback"/>
-      <Button  handleClick={SetIncreaseByOneGood} text="good" />
-      <Button  handleClick={SetIncreaseByOneNeutral} text="neutral" />
-      <Button  handleClick={} text="bad" />
+      <Button  handleClick={setIncreaseByOneGood} text="good" />
+      <Button  handleClick={setIncreaseByOneNeutral} text="neutral" />
+      <Button  handleClick={setIncreaseByOneBad} text="bad" />
       <Display value="statistsc"/>
-      <Feedback text="good"  numfeedback={good}/>
-      <Feedback text="neutral"  numfeedback={neutral}/>
-      <Feedback text="bad"  numfeedback={bad}/>
+      <Feedback text="good"  numFeedBack={good}/>
+      <Feedback text="neutral"  numFeedBack={neutral}/>
+      <Feedback text="bad"  numFeedBack={bad}/>
     </div>
   )
 }
