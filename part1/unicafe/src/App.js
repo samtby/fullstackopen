dfe6050609f1e542fe262b4d1fea6bffd0a2e367
NoRoc <samtby@hydrox.fr>
2021-06-24 23:12:20 +0200

    1.7: unicafe step2 problem on "good"

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,21 +15,57 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const setIncreaseByOneGood = () => setGood(good + 1)
-  const setIncreaseByOneNeutral = () => setNeutral(neutral + 1)
-  const setIncreaseByOneBad = () => setBad(bad + 1)
   
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  
+  //the average score (good: 1, neutral: 0, bad: -1) 
+  const averageScore = { good: 1,  neutral: 0, bad: -1 }
+  
+  const setIncreaseByOneGood = () => { 
+    setAll(all + 1)
+    setGood(good + 1)   
+    console.log(good) 
+    setAverage(good+(neutral*averageScore.neutral)+(bad*averageScore.bad))
+  }
+
+  const setIncreaseByOneNeutral = () => {
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+    setAverage(good+(neutral*averageScore.neutral)+(bad*averageScore.bad))
+  }
+  const setIncreaseByOneBad = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+    setAverage(good+(neutral*averageScore.neutral)+(bad*averageScore.bad))
+  }
+
+  const Display = props => <h1>{props.value}</h1>
+
+    const Button = ({handleClick,text}) => (
+      <button onClick={handleClick}>{text}</button>
+    )
+
+    const Feedback =  ({text,numFeedBack}) => (
+      <p>{text} {numFeedBack}</p>
+    )
+
   return (
     <div>
       <Display value="give feedback"/>
+      
+      <Display value={(6+2*0+-1*1)/9}/>
       <Button  handleClick={setIncreaseByOneGood} text="good" />
       <Button  handleClick={setIncreaseByOneNeutral} text="neutral" />
       <Button  handleClick={setIncreaseByOneBad} text="bad" />
       <Display value="statistsc"/>
-      <Feedback text="good"  numFeedBack={good}/>
-      <Feedback text="neutral"  numFeedBack={neutral}/>
-      <Feedback text="bad"  numFeedBack={bad}/>
+      <Feedback text="good"  numFeedBack={good}/><p>{good}</p>
+      <Feedback text="neutral"  numFeedBack={neutral}/><p>{neutral*averageScore.neutral}</p>
+      <Feedback text="bad"  numFeedBack={bad}/> <p>{bad*averageScore.bad }</p>
+      <Feedback text="all"  numFeedBack={all}/>
+      <Feedback text="average"  numFeedBack={average}/>
+      <Feedback text="positive"  numFeedBack={positive}/>
     </div>
   )
 }
