12c4c2d15b7b0a114600627ab487831173f6ea7e
samtby <julien.boulay@hydrox.fr>
2021-06-29 15:42:14 +0200

    formating files

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,7 @@
 import React, { useState } from 'react'
 //The application must display the total number of collected feedback for each category. 
 
-// This is the right place to define a component
-// const Display = ({value}) => <h1>{value}</h1>
-// // This is the right place to define a component
-// const Button = ({handleClick,text}) =><button onClick={handleClick}>{text}</button>
-
 // const Status = (props) => {
-
 //   const [good, setGood] = useState(0)
 //   const [neutral, setNeutral] = useState(0)
 //   const [bad, setBad] = useState(0)
@@ -15,7 +9,6 @@ import React, { useState } from 'react'
 //   const [average, setAverage] = useState(0)
 //   const [positive, setPositive] = useState(0)
 // }
-
 /*
 const Average = (props){
 
@@ -23,7 +16,7 @@ const Average = (props){
   //setPositive(good/(all/100))
   return(
     <div>
-
+      
     </div>
 }
 */
@@ -65,7 +58,6 @@ const FeedbackSystem = ()=>{
     setAll(all + 1)
     setBad(bad + 1)
   }
-//<Statistics  good={good} neutral={neutral} all={all}/>
   return (
     <div>
       <button value={good} onClick={setIncreaseByOneGood}>good</button>
@@ -83,62 +75,11 @@ const FeedbackSystem = ()=>{
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  /*const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)*/
-  
-  //the average score (good: 1, neutral: 0, bad: -1) 
-  //const averageScore = { good: 1,  neutral: 0, bad: -1 }
-  
-  /*const setIncreaseByOneGood = () => {
-    setAll(all + 1)
-    setGood(good + 1)   
-  }
-  const setIncreaseByOneNeutral = () => {
-    setAll(all + 1)
-    setNeutral(neutral + 1)
-  }
-  const setIncreaseByOneBad = () => {
-    setAll(all + 1)
-    setBad(bad + 1)
-  }
-/*
-  const Statistics =  ({text,numFeedBack}) => {
-    setAverage((good + (neutral*0) + bad*(-1))/all)
-    if (text === "average") {
-      return(
-        <p>{text} {average}</p>
-      )
-    }
-    if (text === "positive") {
-        setPositive(good/(all/100))
-      return(
-        <p>positive {positive} %</p>
-      )
-    }
-    return(
-        <p>{text} {numFeedBack}</p>
-    )
-  }*/
   return (
     <div>
       <h1>Feedback</h1>
       <FeedbackSystem/>
-      {/* <Display value="give feedback"/>
-      <Button  handleClick={setIncreaseByOneGood} text="good" />
-      <Button  handleClick={setIncreaseByOneNeutral} text="neutral" />
-      <Button  handleClick={setIncreaseByOneBad} text="bad" />
-      <Display value="statistsc"/>
-      <Statistics text="good"  numFeedBack={good}/>
-      <Statistics text="neutral"  numFeedBack={neutral}/>
-      <Statistics text="bad"  numFeedBack={bad}/> 
-      <Statistics text="all"  numFeedBack={all}/>
-      <Statistics text="average"/>
-      <Statistics text="positive"/> */}
+        {/* comments */}
     </div>
   )
 }
