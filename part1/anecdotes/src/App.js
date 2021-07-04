0f8d1ecc70334be10b67f6c8f68ad8f38eb0c54c
NoRoc <samtby@hydrox.fr>
2021-07-04 15:39:01 +0200

    1.13*: anecdotes step2 in progress

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -4,14 +4,6 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
 
 const App = () => {
 
-  //const points = new Object({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0  })
-   //let points =  new Array([0 ,1 , 2, 3, 4, 5, 6])
-   let points = [0 ,0 , 0, 0, 0, 0, 0]
-
-  
-  let [votes, setVotes] = useState(new Array([0 ,0 , 0, 0, 0, 0, 0]))
-  let copyVotes = { ...votes }
-
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -22,6 +14,25 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
   ]
    
+
+  //const points = new Object({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0  })
+   //let points =  new Array([0 ,1 , 2, 3, 4, 5, 6])
+   const points = new Array([0 ,0 , 0, 0, 0, 0, 0])
+
+   //const points = new Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0);
+  /*
+   Array.apply(null, new Array(7)).map(Number.prototype.valueOf,0);
+   //Output as [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+   
+   new Array(10+1).join('0').split('').map(parseFloat)
+   //Output as [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  */ 
+
+  //let [votes, setVotes] = useState(new Array([0 ,0 , 0, 0, 0, 0, 0]))
+  const [votes, setVotes] = useState(points)
+  const copyVotes = { ...votes }
+
+ 
   const [selected, setSelected] = useState(0)
   const [voteSelected, setSelectedVote] = useState(0)
 
@@ -31,110 +42,11 @@ const App = () => {
   }
 
   const addNote = (props) => {   
-        //updateVotes(votes.splice(props.selected,0,88))
-    //updateVotes(oldArray  => [oldArray, oldArray[selected] += 1])
+    setSelectedVote(votes[0][selected] +=  1)
+    console.log("votes[0]",votes[0])
     
-    //Version noCopy 
-    
-    votes[0][selected] +=  1
-    setSelectedVote(votes[0][selected])
-    //console.log("votes",votes[0][selected])
-    
-    
-    //Version copy
-    //copyVotes[selected]
-    //console.log("votes",copyVotes[selected])
-   /* setVotes(
-      //array.map(function(currentValue, index, arr))
-      //votes.map.set(votes.indexOf(1))
-      /*votes.map((votes) =>{
-        votes.id === selected ? newVote : vo
-      
-
-      )*/
-    
-    //setVotes(votes => votes.se +=1)
-    //  setVotes([...votes,votes[props] =+ 1 //add
-    //setVotes(votes => [ ...votes,votes[props] ])
- 
-    /*console.log(selected)
-    setVotes( array =>  array[selected] += 1) //Fail    
-
-    console.log(votes[selected])
-    */
   }
   
-
-/* Updating Arrays
-  const [items, setItems] = useState([])
-
-  // Completely replaces whatever was stored in the items array
-  setItems([{item1}, {item2}])
-  
-  // Don't use JS array methods such as pop, push, shift, unshift 
-  // as these will not tell React to trigger a re-render. 
-  items.push({item3})
-  
-  // Instead, make a copy of the array then add your new item onto the end
-  setItems([...items, {item3}])
-  
-  // To update an item in the array use .map. 
-  // Assumes each array item is an object with an id.
-  setItems(
-    items.map((item, index) => {
-      item.id === id ? newItem : item
-    })
-  )
-
-  */
-  
-  //Update Objects 
-/*
-  const Person = () => {
-    const [person, setPerson] = useState({
-      firstName: '',
-      lastName: ''
-    });
-  
-    const handleChange = (e) => {
-      setPerson({
-        ...person,
-        [e.target.name]: e.target.value
-      });
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      // Form submission logic here.
-    }
-  
-    return (
-      <form>
-        <label htmlFor='first'>
-          First Name:
-          <input
-            id='first'
-            name='firstName'
-            type='text'
-            value={person.firstName}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor='last'>
-          Last Name:
-          <input
-            id='last'
-            name='lastName'
-            type='text'
-            value={person.lastName}
-            onChange={handleChange}
-          />
-        </label>
-        <button type='submit' onClick={handleSubmit}>Submit</button>
-      </form>
-    );
-  };
-*/  
   return (
     <div>
       <table>
