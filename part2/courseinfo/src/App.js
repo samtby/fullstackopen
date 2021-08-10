579e66247120cb353120fb7475b979c7201266f2
NoRoc <samtby@hydrox.fr>
2021-08-10 21:28:01 +0200

    2.1: Course information step6 started

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,70 @@
+const Header = (props) => {
+  console.log("Header",props)
+  return (
+  <>
+    <h1>{props.course.name}</h1>
+  </>
+  )
+}
+
+const Content  = (props) => {
+  
+  console.log("Contect",props)
+  return (
+  <div>
+    <Part parts={props.parts.parts[0].name}  exercises={props.parts.parts[0].exercises}/>
+    <Part parts={props.parts.parts[1].name}  exercises={props.parts.parts[1].exercises}/>
+    <Part parts={props.parts.parts[2].name}  exercises={props.parts.parts[2].exercises}/>
+  </div>
+  )
+}
+
+const Part  = (props) => {  
+  console.log("Part",props)
+  return (
+  <>
+    <p>
+      {props.parts} {props.exercises}
+    </p>
+  </>
+  )
+}
+
+const Total  = (props) => {  
+  console.log("Total",props)
+  return (
+  <>
+    <p>Number of exercises {props.parts.parts[0].exercises + props.parts.parts[1].exercises +props.parts.parts[2].exercises}</p>
+  </>
+  )
+}
+ 
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course}/>
+      <Total parts={course} /> 
+      <arto />
+    </div>
+  )
+}
+
+export default App
