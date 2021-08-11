e0c880e7236b34e24378e41a18c1336da9926e0a
samtby <julien.boulay@hydrox.fr>
2021-08-11 14:56:00 +0200

    2.4: Course information step9 complete

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,46 +1,30 @@
   const Header = ({course}) =><h3>{course.name}</h3>
   
-  const Parts  = ({parts}) =>parts.map(parts => parts.parts.map(parts =>  <Part key={parts.id} parts={parts.name}  exercises={parts.exercises}/>))
-    
-  const Content = ({parts}) => {
-    return (
-        <div>
-            {parts.map(part => 
-                <p key={part.id}>
-                {part.name} {part.exercises}
-                </p>
-            )}
-        </div>
-        )        
-  }
+  const Content  = ({parts}) =>parts.map(parts => <Part key={parts.id} name={parts.name}  exercises={parts.exercises}/>)
 
-  const Part  = ({parts,exercises}) => <p>{parts} {exercises}</p>
+  const Part  = ({name,exercises}) => <p>{name} {exercises}</p>
+  
   const Total  = ({parts}) => {              
-    const total = function total(sum, val) {
-        //console.log(sum, val,'somme', sum + val)
-        return sum + val;
-    }
+    const total = function total(sum, val) { return sum + val; }
+
     return (
-   <div>
-        <h4>total of exercises {parts.map(course => course.parts.map(li => li.exercises).reduce(total,0)).reduce(total,0)}</h4>
-   </div>
+    <div>
+        <h4>total of exercises {parts.map(li => li.exercises).reduce(total,0)}</h4>  
+    </div>
     )
   }
 
   const Course = ({course}) => {
-      //<Header course={course} />
     return (
     <div>
+        <h2>Web development curriculum</h2>
         {course.map(courses  => 
         <li>
             <Header course={courses}/>
             <Content parts={courses.parts}/>
-
+            <Total parts={courses.parts}/>      
         </li>
         )}
-        
-        {/*<Content parts={course}/>
-        <Total parts={course} /> */}
     </div>
     )
 }
