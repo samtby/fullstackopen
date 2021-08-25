909992e5b0f44eb5e165aef5e3d769ea10cb82fd
samtby <julien.boulay@hydrox.fr>
2021-08-25 16:47:17 +0200

    2.12* Data for countries, step1(over 10) countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -6,20 +6,28 @@ import axios from 'axios'
 const App = () => {
   const [ newFilter, setNewFilter ] = useState('')
   const [ countries, setCountries ] = useState([]) 
-
+  const [error, setError] = useState('');
   const initialStateCountries = []
     useEffect(() => {
       console.log('effect')
         if(newFilter !== ''){
           axios
           .get(`https://restcountries.eu/rest/v2/name/${newFilter}`)
-          .then(response => {
-            console.log('promise fulfilled')
+          .then(
+            response => {
+              console.log('response status',response)
+              
+            //console.log('promise fulfilled',response)
+            
             setCountries(response.data)
           })
+          .catch((err) => {
+            console.log('err',err)
+            setError(err);
+        })
         console.log('effect',countries)
       }
-    }, [newFilter])// Only re-run the effect if newFilter or countries changes
+    }, [newFilter])// Only re-run the effect if newFilter changes
     
   const handleFilterChange = (event) => {
     if(event.target.name === 'filter')
--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,17 +1,13 @@
 //const Countries = ({countries,filter}) =><div>{countries.filter(name =>name.name.toUpperCase().includes(filter.toUpperCase())).map(person =><li key={person.name.toString()}> {person.name } {person.number}</li>)}</div>
 const Countries = ({countries,filter}) =>{
     const prop = [countries,filter]
-    console.log('countries',  countries.lenght)
+    console.log('prop',  prop[0].length)
     
-  const isMoreTenResult = () => {
-    var test = null
-     
     
-  };
-
+    console.log('countries',countries.length)
 return (
     <div>
-        {countries.map(person =><li key={person.name.toString()}> {person.name } {person.number}</li>)}
+        { countries.length <=10? countries.map(person =><li key={person.name.toString()}> {person.name } {person.number}</li>): <p>Too many matches, specify another filter</p>}
     </div>
     )
 }
