c7d6da5ed57d89da9392764c2396f9d840ad2d1b
samtby <julien.boulay@hydrox.fr>
2021-09-01 09:00:59 +0200

    in progres

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,12 +1,35 @@
 //const Countries = ({countries,filter}) =><div>{countries.filter(name =>name.name.toUpperCase().includes(filter.toUpperCase())).map(person =><li key={person.name.toString()}> {person.name } {person.number}</li>)}</div>
 import React, {useState,useEffect} from 'react'
+import axios from 'axios'
 const Countries = ({countries,filter}) =>{
-    const [ currentCountry, setcurrentCountry ] = useState('')   
+    const [ currentCountry, setcurrentCountry ] = useState('')
+    const [ currentWeather, setcurrentWeather ] = useState([])
+    const api_key = process.env.REACT_APP_API_KEY
     useEffect(() => {    
       setcurrentCountry('')
+      setcurrentWeather([])
     }, [filter])
-  
-    if(currentCountry !==''){
+
+    useEffect(() => {
+      if(currentCountry !==''){    
+        const capital = countries.filter(counties => counties.alpha3Code === currentCountry).map(country =>country.capital.toString())
+
+    axios
+    .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`)
+    .then(
+      response => {
+        //console.log('response status',response)            
+      setcurrentWeather(response.data)
+    })
+    .catch((err) => {
+      //console.log('err',err)            
+  })
+  }else{
+    setcurrentWeather([])
+  }
+}, [api_key,countries,currentCountry])/// Only re-run the effect if newFilter changes
+
+    if(currentCountry !==''){    
     return (
       <div>
         {countries.filter(counties => counties.alpha3Code === currentCountry)
@@ -19,9 +42,9 @@ const Countries = ({countries,filter}) =>{
                 <ul>
                   {country.languages.map(language => <li key={language.name.toString()}>{language.name}</li>)}
                 </ul>                
-                <img  src={country.flag} alt="flag"  height="87px"  width="100px"/>                
-            </div>)
-            }
+                <img  src={country.flag} alt="flag"  height="87px"  width="100px"/>    
+                <h2>Weather in {country.capital}</h2>      
+            </div>)}            
         </div>
       )      
   }else if(countries.length ===1 ){
