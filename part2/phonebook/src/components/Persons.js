0ec1d88de221d383ad47d1fb96b277bbd88424cc
NoRoc <julien.boulay@epsi.fr>
2021-09-09 08:43:39 +0200

    2.17: Phonebook step9 complete in progress

--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -2,19 +2,24 @@ import personService from '../services/persons'
 //const Persons = ({persons,filter}) =><div>{persons.filter(name =>name.name.toUpperCase().includes(filter.toUpperCase())).map(person =><li key={person.name.toString()}> {person.name } {person.number}<button  type="button" onClick={()=>personService.del(person.id).then(response =>{console.log("Reponse",response)})}>delete</button></li>)}</div>
 const Persons = ({persons,filter,handleGetAll}) =>{
 
-const deletePerson = (id) => {
-    personService.del(id).then(response =>{response.status === 200?console.log(response.status):console.log(response.status)})
+const deletePerson = (id,name) => {
+    
 
 /*    const allPersones = []
     allPersones = personService.getAll() **/
-    console.log(personService.getAll())
+    //console.log(personService.getAll())
+
+    if (window.confirm(`Delete ${name} ?`)) {
+        personService.del(id).then(response =>{response.status === 200?console.log(response.status):console.log(response.status)})    
+    }
+
 }
 return (
     <div>
         {persons.filter(name =>name.name.toUpperCase().includes(filter.toUpperCase()))
             .map(person =>
                 <li key={person.name.toString()}> {person.name } {person.number}
-                    <button  type="button" onClick={()=>deletePerson(person.id)}>delete</button>
+                    <button  type="button" onClick={()=>deletePerson(person.id,person.name)}>delete</button>
                 </li>
                 )}
     </div>)
