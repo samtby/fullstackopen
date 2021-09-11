52e32ff9f75a5f0645a70f648bafa9d61b23e07c
NoRoc <samtby@hydrox.fr>
2021-09-11 10:53:23 +0200

    2.18*: Phonebook step10 correction

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -3,34 +3,32 @@ import personService from '../services/persons'
 const PersonForm = ({persons,name,number,handlePerson,handlePhone,handleSetPersons,handleSetName,handleSetNumber,handleGetAll,handleNotif}) => {
 //handleAddPerson
   const addPerson = (event) => {    
-        event.preventDefault()
-          const person = persons.filter(person => person.name === name)
-          console.log("person",person)
-          if(person.some(person => person.name === name)){
-            //console.log("Object found inside the array.");       
-                     
-              if(person.some(person => person.number !== number)){
-                console.log("id",person[0].id)                
-                    if (window.confirm(`${name} is already added is already added to phonebook, replace the old number with a new one?`))
-                      personService.update(person[0].id,{ name , number })
-                      .then(returnedPerson =>{
-                        handleSetPersons(persons.map(person => person.id !== returnedPerson.id ? person: returnedPerson))
-                        }
-                      )
-                      //.then(response =>{handleGetAll()})
-              }
-          }else{              
-              console.log("Object not found.")
-              personService
-                .create({ name , number })
-                .then(returnedPerson  => {handleSetPersons(persons.concat(returnedPerson))
-                  handleNotif(`Added '${name}`)
-                  handleSetName()
-                  handleSetNumber()
-                })
-                //.then(response =>response !== undefined?handleGetAll():console.log("Create error status."))              
-           }
+     event.preventDefault()
+      const person = persons.filter(person => person.name === name)
+      if(person.length === 1){
+        if(person.some(person => person.name === name)){
+          if(person.some(person => person.number !== number)){   
+            if (window.confirm(`${name} is already added is already added to phonebook, replace the old number with a new one?`))
+              personService.update(person[0].id,{ name , number })
+              .then(returnedPerson =>{ handleSetPersons(persons.map(person => person.id !== returnedPerson.id ? person: returnedPerson))
+                handleSetName()
+                handleSetNumber()
+              })
+              //.then(response =>{handleGetAll()})
+        }else{              
+            console.log("Object not found.")
+            personService
+              .create({ name , number })
+              .then(returnedPerson  => {handleSetPersons(persons.concat(returnedPerson))
+                handleNotif(`Added '${name}`)
+                handleSetName()
+                handleSetNumber()
+              })
+              //.then(response =>response !== undefined?handleGetAll():console.log("Create error status."))              
+        }
       }
+    }
+  }
     return (
     <div>
       <form onSubmit={addPerson}>
