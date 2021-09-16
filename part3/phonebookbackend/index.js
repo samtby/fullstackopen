6a985996f7e85491cb9d930daf3936ed28114585
NoRoc <julien.boulay@epsi.fr>
2021-09-16 13:51:09 +0200

    3.2: Phonebook backend step2 complete

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -64,54 +64,20 @@ app.post('/api/persons', (request, response) => {
    
   })
 
+app.get('/api/info', (request, response) => {
+  responseTime =  request.responseTime
 
-  app.get('/api/info', (request, response) => {
-    responseTime =  request.responseTime
-    response.send(
-      '<p>Phonebook has info for '+ persons.length+' people</p>'+
-      '<p>Phonebook has info for '+ persons.length+' people</p>'    
-    )
-    let date_ob = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    let dateFinal = date_ob.toLocaleString("en-US",options);
+  let date_ob = new Date();
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  let dateFinal = date_ob.toLocaleString("en-US",options);
 
-    let timeString =date_ob.toTimeString();
-    // current date
-    // GTM
-    let gtm = date_ob.toGMTString();
-
-    let day = date_ob.getDay();
-    // adjust 0 before single digit date
-    let date = ("0" + date_ob.getDate()).slice(-2);
-    
-    // current month
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    
-    // current year
-    let year = date_ob.getFullYear();
-    
-    // current hours
-    let hours = date_ob.getHours();
-    
-    // current minutes
-    let minutes = date_ob.getMinutes();
-    
-    // current seconds
-    let seconds = date_ob.getSeconds();
-    
-    // prints date in YYYY-MM-DD format
-    console.log(year + "-" + month + "-" + date);
-    
-    // prints date & time in YYYY-MM-DD HH:MM:SS format
-    console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
-    
-    // prints time in HH:MM format
-    console.log(hours + ":" + minutes);
-    
-    console.log(day+"_"+year + "-" + month + "-" + date);
-    
-    console.log(dateFinal+" "+ month + " "+day+" "+year+timeString);
-  })
+  let timeString =date_ob.toTimeString();
+  // current date
+  response.send(
+    '<p>Phonebook has info for '+ persons.length+' people</p>'+
+    '<p>'+dateFinal+' '+timeString+'</p>'    
+  )
+})
   
 const PORT = 3001
 app.listen(PORT, () => {
