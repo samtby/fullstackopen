96ea4bc011360225baa30cfd6ea2c6a793c0f849
NoRoc <samtby@hydrox.fr>
2021-09-19 18:56:48 +0200

    3.15: Phonebook database, step3-4 complete

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -9,16 +9,24 @@ morgan.token('body',(req)=> JSON.stringify(req.body));
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json());
-app.use(morgan('tiny'));
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError')
+    return response.status(400).send({ error: 'malformatted id' })
+  next(error)
+}
+
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'));
 
 const Persons = require('./models/person.js')
-
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response,next) => {
+
   Persons.find({})
   .then(person => {
     response.json(person)
@@ -26,26 +34,7 @@ app.get('/api/persons', (request, response) => {
   .catch(error => next(error))
 })
 
-
-
-
 app.get('/api/persons/:id', (request, response, next) => {    
-/*
-  var mongoose = require('mongoose');
-  var myId = new mongoose.Types.ObjectId(request.params.id)
-  //var mongoose = require('mongoose');
-  //let id = new mongoose.mongo.ObjectId(request.params.id);
-  
-  Persons.find({_id:request.params.id}, function (err, user) {   console.log('error :', err.message) } );
-  */
-
-  //Good id
-  //53cb6b9b4f4ddef1ad47f94f
-/*
-  const mongoose = require('mongoose');
-const id = mongoose.Types.ObjectId (request.params.id)
-*/
-
 Persons.findById(request.params.id)    
     .then(
       person =>{console.log(person)
@@ -56,27 +45,13 @@ Persons.findById(request.params.id)
     })
     .catch(error => next(error))    
 })
-/*
-//app.put('/api/persons/:id'),(request, response) => {}
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.findIndex(person => person.id === id);
-  if(person !== -1){
-    const delPerson = persons.splice(person,1);
-    if(delPerson.map(del => del.id === id))
-      response.json(delPerson);
-    else
-      response.status(500).send('Internal Server Error');
-  }else
-    response.status(404).send('Not fund');
-})
-*/
-app.post('/api/persons',morgan(':method :url :status :res[content-length] :response-time ms :body'), (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const personreq = Object();
    Object.assign(personreq ,request.body);
     if((personreq.name.length*personreq.number.length) !== 0){
-      Persons.find({name:personreq.name}).then(person => {
+      Persons.find({name:personreq.name})
+      .then(person => {
         //if(person.length === 0){
           const pers = new Persons({name: personreq.name, number: personreq.number })
           pers.save()
@@ -85,36 +60,62 @@ app.post('/api/persons',morgan(':method :url :status :res[content-length] :respo
             response.json(result)
           })
           .catch(error => next(error))
-      /*  }else
-          response.status(400).json({ error: 'name must be unique' });*/
-      })          
+      })
+      .catch(error => next(error))         
     }else
-      response.status(400).json({ error: 'The name or number is missing' });
+      response.status(400).json({ error: 'The name or number is missing' });    
 })
-/*
-app.get('/api/info', (request, response) => {
 
-  let date_ob = new Date();
-  let dateFinal = date_ob.toLocaleString("en-US",{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-  let timeString =date_ob.toTimeString();
-  response.send(
-    '<p>Phonebook has info for '+ persons.length+' people</p>'+
-    '<p>'+dateFinal+' '+timeString+'</p>'    
-  )
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = Number(request.params.id);
+  Persons.findByIdAndRemove(request.params.id)
+  .then(
+    person =>{console.log(person)
+    if(person === null)
+      response.status(404).send("The person no exist")
+    else
+      response.json(person)
+  })
+  .catch(error => next(error))
 })
-*/
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Persons.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+app.get('/api/info', (request, response, next) => {
+  Persons.find({})
+  .then(persons => {    
+    persons.length
+    let date_ob = new Date();
+    let dateFinal = date_ob.toLocaleString("en-US",{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    let timeString =date_ob.toTimeString();
+    response.send(
+      '<p>Phonebook has info for '+ persons.length+' people</p>'+
+      '<p>'+dateFinal+' '+timeString+'</p>'    
+    )
+  })
+  .catch(error => next(error))
+  
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 }
 app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError')
-    return response.status(400).send({ error: 'malformatted id' })
-  next(error)
-}
-
 // this has to be the last loaded middleware.
 app.use(errorHandler)
 
