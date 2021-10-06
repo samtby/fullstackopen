5f971139bb51b9e22e3a6609b8107d2f490ed4c0
samtby <julien.boulay@hydrox.fr>
2021-10-06 12:28:21 +0200

    in progres test

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,6 +5,22 @@ const dummy = (blogs) => {
     //return 0
   }  
 
+  const totalLikes = (blogs) => {
+  let countLikes = 0
+    /*Object.keys(blogs).map(function(likes, index) {
+      countLikes =+ blogs[likes] 
+    })*/
+
+    /*
+  for (var key in myObject) {
+    if (myObject.hasOwnProperty(likes))
+      countLikes += blogs[likes]
+  */
+
+    blogs.map(blog => countLikes += blogs.totalLikes)
+  return countLikes
+  }
+  
   module.exports = {
     dummy
   }
