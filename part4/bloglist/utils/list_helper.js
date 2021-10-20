1758e7db11fdac8daa96cc6d3572b489c91a355d
NoRoc <samtby@hydrox.fr>
2021-10-20 19:41:21 +0200

    in progress lodash

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+// Load the full build.
+var _ = require('lodash');
 const dummy = (blogs) => {
   // ...
   if(Array.isArray(blogs))
@@ -26,6 +28,43 @@ console.log(favoriteBlog["likes"])
   return {title: favoriteBlog["title"], author: favoriteBlog["author"], likes: favoriteBlog["likes"]}
 }
 
+const mostBlogs = (blogs) => {
+  //arr2 = findOcc(blogs,"author");
+  //New technical with 
+  let mostBlog = _.countBy(blogs,"author")  
+  mostBlog.reduce(function(prev, current) {
+    return (prev.likes > current.likes) ? prev : current
+  }) //returns object  
+  //Old technical
+  //const mostBlog = _.countBy(blogs,"author");
+  console.log("countBy mostBlog: ",mostBlog)
+}
+//Old technical
+function findOcc(arr, key){
+  let arr2 = [];
+  arr.forEach((x)=>{
+       
+    // Checking if there is any object in arr2
+    // which contains the key value
+     if(arr2.some((val)=>{ return val[key] == x[key] })){
+       // If yes! then increase the occurrence by 1
+       arr2.forEach((k)=>{
+         if(k[key] === x[key])
+           k["occurrence"]++
+      })         
+     }else{
+       // If not! Then create a new object initialize 
+       // it with the present iteration key's value and 
+       // set the occurrence to 1
+       let a = {}
+       a[key] = x[key]
+       a["occurrence"] = 1
+       arr2.push(a);
+     }
+  })
+  return arr2
+}
+
 module.exports = {
-dummy,totalLikes,favoriteBlog
+  dummy,totalLikes,favoriteBlog,mostBlogs
 }
