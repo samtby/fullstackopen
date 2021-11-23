bbdb0f0ee26468c726d38e3018c63aba5d302ae7
samtby <julien.boulay@hydrox.fr>
2021-11-23 18:07:35 +0100

    Token authentication in progress

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -18,8 +18,9 @@ const errorHandler = (error, request, response, next) => {
     if (error.name ==='CastError')
       return response.status(400).send({ error: 'malformatted id' })
     if (error.name ==='ValidationError')
-      return response.status(400).json({ error: error.message })
-    
+      return response.status(400).json({ error: error.message })    
+    else if (error.name === 'JsonWebTokenError')
+      return response.status(401).json({ error: 'invalid token' })
     next(error)
 }
 
