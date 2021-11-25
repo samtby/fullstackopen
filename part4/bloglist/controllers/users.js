1e0759e825896a65a182fccc04876aacf9338fa4
NoRoc <julien.boulay@epsi.fr>
2021-11-25 13:59:21 +0100

    4.15: bloglist expansion, step3 complete

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,17 +2,6 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-
-/** on postman */
-
-/*
-  {
-      "notes": [],
-      "username": "root",
-      "name": "Superuser",
-      "password": "salainen"
-  }
-*/
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({}).populate('blogs',{title: 1,author: 1})
   response.json(users)
