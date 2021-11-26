5efe4904cbf98b4dc8f12e803fa0feb8cbc7cddf
NoRoc <samtby@hydrox.fr>
2021-11-26 22:53:04 +0100

    4.20*: bloglist expansion, step8 in progress

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -6,12 +6,6 @@ const jwt = require('jsonwebtoken')
 
 const blogsRouter = express();
 
-const getTokenFrom = request => {  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) 
-    return authorization.substring(7) 
-  return null
-}
-
 blogsRouter.get('/', async (request, response,next) => {
       //const blogs = await Blog.find({})
       const blogs = await Blog.find({}).populate('user',{username: 1,name: 1})
@@ -42,8 +36,8 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const token = getTokenFrom(request)  
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  //console.log("request.token : ",request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!token || !decodedToken.id) 
     return response.status(401).json({ error: 'token missing or invalid' })
 
