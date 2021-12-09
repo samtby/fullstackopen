87eab23c859dd4055d92d32cdbc3c512c27a6c9a
NoRoc <julien.boulay@epsi.fr>
2021-12-09 08:07:47 +0100

    in progress

--- part5/bloglist-frontend/src/components/Blogs.test.js
+++ part5/bloglist-frontend/src/components/Blogs.test.js
@@ -2,15 +2,37 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import Blog from './Blog'
+import loginService from '../services/login'
+import blogService from '../services/blogs'
 
-test('renders content', () => {
+
+test('renders content', async () => {
+
+  let user
+  console.log('logging in with mluukkai')
+  
+    const responseLogin = await loginService.login({
+      username: 'mluukkai',
+      password: 'salainen'
+    })
+
+    responseLogin.then(function(result) {
+      console.log('responseLogin: ',result) // "Some User token"
+   })
+    //console.log('responseLogin: ' ,responseLogin)
+    /*window.localStorage.setItem(
+      'loggedBlogappUser', JSON.stringify(user)
+    ) // You can view localstorage by console 'window.localStorage'*/
+     blogService.setToken(responseLogin.token)
+
+
   const blog = {
     title: 'async/await simplifies making async calls',
     author: 'Robert C. Martin'
   }
 
   const component = render(
-    <Blog blog={blog} />
+    <Blog blog={blog}user={responseLogin} />
   )
 
   expect(component.container).toHaveTextContent(
